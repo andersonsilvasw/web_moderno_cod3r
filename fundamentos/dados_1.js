b3bdf2bfaaf458bdceb301e42035a673a1c2468e
andersonsilvasw <andersonsilvaedif@hotmail.com>
2023-06-21 18:54:49 -0300

    First Commit

---
+++ fundamentos/dados_1.js
@@ -0,0 +1,14 @@
+var nome = "canteta";
+var quantidade = 10;
+var preco = 6.40;
+let imposto = 1.50;
+let precoFinal = preco + imposto
+
+console.log(nome);
+console.log(quantidade);
+console.log(preco);
+console.log(imposto);
+console.log(precoFinal);
+
+nome = "caneta BIC";
+console.log(nome);
