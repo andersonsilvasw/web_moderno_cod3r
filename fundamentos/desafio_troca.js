6de31003724ed85c87af82727b1b76ccf8e9a5b9
andersonsilvasw <andersonsilvaedif@hotmail.com>
2023-06-22 17:22:10 -0300

    Desafio troca de variáveis

---
+++ fundamentos/desafio_troca.js
@@ -0,0 +1,11 @@
+let a  = 7;
+let b = 94;
+
+let temp = a;
+a = b;
+b = temp;
+
+// [a, b] = [b, a]; Essaé a forma mais indicada para trocar os valores das variáveis
+
+console.log(a);
+console.log(b);
