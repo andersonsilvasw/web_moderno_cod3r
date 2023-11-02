c9a1b597b3ba769b9f1938185b90d3defc731ca9
andersonsilvasw <andersonsilvaedif@hotmail.com>
2023-11-02 13:36:19 -0300

    Exercício 4 de 40 da sessão

---
+++ exercicios/exercicio4.js
@@ -0,0 +1,7 @@
+function divisao (dividendo, divisor) {
+    console.log("Resultado: " + Math.floor(dividendo/divisor)); // Math.floor arredonda para baixo
+    console.log(`Resto: ${dividendo % divisor}`)
+}
+
+divisao(11, 4)
+divisao(6, 3)
