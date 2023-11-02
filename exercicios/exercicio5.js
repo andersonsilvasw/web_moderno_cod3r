1e12a5f9d12ad706191536eb95db937941cede53
andersonsilvasw <andersonsilvaedif@hotmail.com>
2023-11-02 19:09:51 -0300

    Exercício 5 de 40 da sessão

---
+++ exercicios/exercicio5.js
@@ -0,0 +1,8 @@
+function formatarValorDecimal(valorDecimal) {
+    valorEmReais = `R$ ${valorDecimal.toFixed(2).toString().replace(".", ",")}`
+    console.log(valorEmReais)
+}
+
+formatarValorDecimal(0.1 + 0.2)
+formatarValorDecimal(2.5)
+formatarValorDecimal(9.75 + 8.56)
