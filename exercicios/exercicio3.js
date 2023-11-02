74da17d4013a902216eea5cdfc3bfc1f51501e30
andersonsilvasw <andersonsilvaedif@hotmail.com>
2023-11-02 13:22:55 -0300

    Exercício 3 de 40 da sessão

---
+++ exercicios/exercicio3.js
@@ -0,0 +1,8 @@
+function expoente(base, expoente) {
+    // Método antigo resultado = Math.pow(base, expoente)
+    // Método Novo abaixo
+    let resultado = base ** expoente
+    return resultado
+}
+
+console.log(expoente(2, 3))
