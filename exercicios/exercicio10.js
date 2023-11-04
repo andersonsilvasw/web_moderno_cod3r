4daf708c1cca03a4779d596ae78453ada99ce35f
andersonsilvasw <andersonsilvaedif@hotmail.com>
2023-11-03 21:34:33 -0300

    Exercício 10 de 40 da sessão

---
+++ exercicios/exercicio10.js
@@ -0,0 +1,12 @@
+function divisivelPorTres(numero) {
+    if (numero % 3 == 0) {
+        return true
+    } else {
+        return false
+    }
+}
+
+console.log(divisivelPorTres(-6))
+console.log(divisivelPorTres(2))
+console.log(divisivelPorTres(3))
+console.log(divisivelPorTres(120))
